d28d6255786e028d188d308ef44087784844f07d
AhmedSalheia <AhmedSalheia.as@gmail.com>
2020-09-01 14:11:14 +0300

    V1.0

---
+++ app/languages/en/notfound/notfound.lang.php
@@ -0,0 +1,3 @@
+<?php
+
+$_['title'] = 'Baraa Zoroub - Not Fount Page';
--- app/views/notfound/notfound.view.php
+++ app/views/notfound/notfound.view.php
@@ -1,2 +1,9 @@
 <?php header("HTTP/1.0 404 Not Found"); ?>
-<h1>Sorry.... The requested page was not found</h1>
+
+<div class="404 w-100 d-flex justify-content-center align-items-center" style="background: url(<?= IMG ?>404.png) no-repeat center center;background-size: 101% 101%; height:100vh;">
+    <div class="text text-center mb-5" style="position:absolute; top:59%!important">
+        <h1 class="text-primary mb-2" style="font-size: 55px; font-weight:bold;line-height: 1;">OoOps!</h1>
+        <p class="lead text-light mb-5" style="font-size:24px;">Something went wrong, Please try again</p>
+        <a href="/" class="btn btn-primary text-light py-3" style="font-weight: 600;font-size: 25px;padding-left: 4rem;padding-right: 4rem;border-radius: 10px;">GO HOME</a>
+    </div>
+</div>
