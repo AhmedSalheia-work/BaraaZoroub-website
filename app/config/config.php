7a09e984e6fe13944956eb6f0ed7c74cb0b0a4d2
AhmedSalheia <AhmedSalheia.as@gmail.com>
2020-09-01 11:27:29 +0300

    V1.0

--- app/config/config.php
+++ app/config/config.php
@@ -15,10 +15,10 @@ define('IMG', '/assets/images/');
 @define('INI', './app/languages/'.$_SESSION['lang'].'/ini/');
 define('UPL', '/assets/uploads/');
 
-defined('DATABASE_HOST_NAME')? null : define('DATABASE_HOST_NAME','localhost'); //business29.web-hosting.com
+defined('DATABASE_HOST_NAME')? null : define('DATABASE_HOST_NAME','business29.web-hosting.com'); //business29.web-hosting.com
 defined('DATABASE_DB_NAME')? null : define('DATABASE_DB_NAME','progwlfo_baraasite');
-defined('DATABASE_USER_NAME')? null : define('DATABASE_USER_NAME','root'); //progwlfo_baraa 
-defined('DATABASE_PASSWORD')? null : define('DATABASE_PASSWORD',''); //baraa@prography.co 
+defined('DATABASE_USER_NAME')? null : define('DATABASE_USER_NAME','progwlfo_baraa'); //progwlfo_baraa 
+defined('DATABASE_PASSWORD')? null : define('DATABASE_PASSWORD','baraa@prography.co '); //baraa@prography.co 
 defined('DATABASE_PORT_NUMBER')? null : define('DATABASE_PORT_NUMBER',3306);
 defined('DATABASE_CONN_DRIVER')? null : define('DATABASE_CONN_DRIVER',1);
 
